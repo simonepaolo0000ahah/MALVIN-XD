18ebea8e1cbfdaebc9bdde0ba868f300100b46f4
simonepaolo0000ahah <simonepaolo0000@gmail.com>
2025-09-05 09:56:49 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   
-  OWNER_NUMBER: "3922605047", // put your phone number here
+  OWNER_NUMBER: "393922605047", // put your phone number here
   
   SESSION_ID: "malvin~h3UkwAAK#r2uJ3WnRQxUMjCipcDrHDfYy_BhMPDfmoleqkaEctGk",  // add your Session Id here
   
