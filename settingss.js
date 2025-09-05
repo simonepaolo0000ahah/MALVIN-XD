d240a9dd97f84befd12ecd5671c535db0c76f2fc
simonepaolo0000ahah <simonepaolo0000@gmail.com>
2025-09-05 09:39:27 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,11 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "393922605047", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  SESSION_ID: "malvin~h3UkwAAK#r2uJ3WnRQxUMjCipcDrHDfYy_BhMPDfmoleqkaEctGk",  // add your Session Id here
+  
+  PREFIX: "+", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
